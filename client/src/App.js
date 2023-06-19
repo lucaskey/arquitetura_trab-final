0ea82f53d46c9db24c415f6dbd232b9517a01beb
Igor Vicentini Garcia <38511932+igorvgarcia@users.noreply.github.com>
2023-06-19 16:13:12 -0300

    feat/adicionando formulario para lançar pedido e incluido radio button criado

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,98 @@
 import "./App.css";
+import RadioButton from "./components/RadioButton";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { faLaptop, faShirt, faGamepad } from '@fortawesome/free-solid-svg-icons';
+
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [pedidos, setPedidos] = useState([]);
+  const [selectedProductType, setSelectedProductType] = useState('');
+  const [formulario, setFormulario] = useState({
+    usuario: '',
+    produto: '',
+    tipoProduto: '',
+  });
+
+  const handleSelect = (value) => {
+    setSelectedProductType(value);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(selectedProductType)
+    setFormulario((prevState) => ({
+      ...prevState,
+      tipoProduto: selectedProductType,
+      [name]: value,
+    }));
+
+  };
 
   useEffect(() => {
-    // Listener para receber notificações de novos pedidos
     socket.on('novoPedido', (novoPedido) => {
       setPedidos((prevPedidos) => [...prevPedidos, novoPedido]);
     });
 
     return () => {
-      // Remova o listener quando o componente for desmontado
       socket.off('novoPedido');
     };
   }, []);
 
   const handleCriarPedido = () => {
-    // Emita um evento para criar um novo pedido
-    socket.emit('criarPedido');
+    socket.emit('criarPedido', formulario);
   };
 
   return (
     <div className="App">
-      <h1>Sistema de Vendas</h1>
-      <button onClick={handleCriarPedido}>Criar Pedido</button>
-      <ul>
-        {pedidos.map((pedido) => (
-          <li key={pedido.id}>Pedido #{pedido.id} - Status: {pedido.status}</li>
-        ))}
-      </ul>
+      <div className="container">
+        <div className="form-container">
+          <div>
+            <div>
+              <h2 className="container-title">Cadastrar pedido</h2>
+            </div>
+            <div>
+              <input name="usuario" type="text" placeholder="Nome do usuário*" class="input" onChange={handleChange} />
+            </div>
+            <div>
+              <input name="produto" type="text" placeholder="Produto*" class="input" onChange={handleChange} />
+            </div>
+            <label>Tipo de produto: </label>
+            <div className="radio-group">
+              <RadioButton
+                icon={faShirt}
+                selected={selectedProductType === "Roupas"}
+                onSelect={() => handleSelect('Roupas')}
+              />
+              <RadioButton
+                icon={faLaptop}
+                selected={selectedProductType === 'Eletrônicos'}
+                onSelect={() => handleSelect('Eletrônicos')}
+              />
+              <RadioButton
+                icon={faGamepad}
+                selected={selectedProductType === 'Brinquedos'}
+                onSelect={() => handleSelect('Brinquedos')}
+              />
+            </div>
+            <div>
+              <button className="btn" onClick={handleCriarPedido}>Criar pedido</button>
+            </div>
+          </div>
+        </div>
+        <div className="list-container">
+          <div>
+            <h2 className="container-title">Pedidos</h2>
+          </div>
+          <ul>
+            {pedidos.map((pedido) => (
+              <li key={pedido.id}>Pedido #{pedido.id} - Status: {pedido.status} - usuario: {pedido.usuario}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
